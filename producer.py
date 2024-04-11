2dde5d95bc35676745cab018d931cbb2a5153c90
automationdream <automationdream@gmail.com>
2024-04-11 22:01:53 +0200

    create simple producer

--- producer.py
+++ producer.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, Consumer
+from confluent_kafka import Producer
 from dotenv import load_dotenv
 import os
 
@@ -18,7 +18,6 @@ config = {
 # Get topic from environment variable
 topic = os.getenv("KAFKA_TOPIC")
 
-
 def produce(topic, config):
   # creates a new producer instance
   producer = Producer(config)
@@ -32,34 +31,5 @@ def produce(topic, config):
   # send any outstanding or buffered messages to the Kafka broker
   producer.flush()
 
-def consume(topic, config):
-  # sets the consumer group ID and offset  
-  config["group.id"] = "python-group-1"
-  config["auto.offset.reset"] = "earliest"
-
-  # creates a new consumer instance
-  consumer = Consumer(config)
-
-  # subscribes to the specified topic
-  consumer.subscribe([topic])
-
-  try:
-    while True:
-      # consumer polls the topic and prints any incoming messages
-      msg = consumer.poll(1.0)
-      if msg is not None and msg.error() is None:
-        key = msg.key().decode("utf-8")
-        value = msg.value().decode("utf-8")
-        print(f"Consumed message from topic {topic}: key = {key:12} value = {value:12}")
-  except KeyboardInterrupt:
-    pass
-  finally:
-    # closes the consumer connection
-    consumer.close()
-
-def main():
+if __name__ == "__main__":
   produce(topic, config)
-  consume(topic, config)
-
-
-main()
