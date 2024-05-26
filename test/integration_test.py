a5301289e26e7456a2377da044470baf5ed19064
automationdream <automationdream@gmail.com>
2024-05-26 23:59:38 +0200

    add several mappings

--- test/integration_test.py
+++ test/integration_test.py
@@ -13,16 +13,28 @@ def wm_server():
     Config.base_url = wm.get_url("__admin") # (2)
     Mappings.create_mapping(
       Mapping(
-        request=MappingRequest(method=HttpMethods.GET, url="/hello"),
-        response=MappingResponse(status=200, body="hello"),
-        persistent=False,
+        request=MappingRequest(method=HttpMethods.GET, url="/world"),
+        response=MappingResponse(status=200, json_body={"response":"world"})
       )
-    ) # (3)
+    )
+    Mappings.create_mapping(
+      Mapping(
+        request=MappingRequest(method=HttpMethods.GET, url="/hello"),
+        response=MappingResponse(status=200, json_body={"response":"hello"})
+      )
+    )
     yield wm
 
-def test_get_hello_world(wm_server): # (4)
+def test_get_hello(wm_server): # (4)
 
-    resp1 = requests.get(wm_server.get_url("/hello"), verify=False)
+    resp1 = requests.get(wm_server.get_url("/hello"))
 
     assert resp1.status_code == 200
-    assert resp1.content == b"hello"
+    assert resp1.json() == {"response":"hello"}
+
+def test_get_world(wm_server): # (4)
+
+    resp1 = requests.get(wm_server.get_url("/world"))
+
+    assert resp1.status_code == 200
+    assert resp1.json() == {"response":"world"}
