55e064cb71708c812887be39e3b5ae302275d88b
automationdream <automationdream@gmail.com>
2024-05-28 17:20:46 +0200

    format

--- test/integration_test.py
+++ test/integration_test.py
@@ -3,38 +3,40 @@ from wiremock.testing.testcontainer import wiremock_container
 from wiremock.constants import Config
 from wiremock.client import *
 import requests
+import time
 
 
-
-@pytest.fixture(scope="session") # (1)
+@pytest.fixture(scope="session")  # (1)
 def wm_server():
-  with wiremock_container(secure=False) as wm:
+    with wiremock_container(secure=False) as wm:
 
-    Config.base_url = wm.get_url("__admin") # (2)
-    Mappings.create_mapping(
-      Mapping(
-        request=MappingRequest(method=HttpMethods.GET, url="/world"),
-        response=MappingResponse(status=200, json_body={"response":"world"})
-      )
-    )
-    Mappings.create_mapping(
-      Mapping(
-        request=MappingRequest(method=HttpMethods.GET, url="/hello"),
-        response=MappingResponse(status=200, json_body={"response":"hello"})
-      )
-    )
-    yield wm
+        Config.base_url = wm.get_url("__admin")  # (2)
+        Mappings.create_mapping(
+            Mapping(
+                request=MappingRequest(method=HttpMethods.GET, url="/world"),
+                response=MappingResponse(status=200, json_body={"response": "world"}),
+            )
+        )
+        Mappings.create_mapping(
+            Mapping(
+                request=MappingRequest(method=HttpMethods.GET, url="/hello"),
+                response=MappingResponse(status=200, json_body={"response": "hello"}),
+            )
+        )
+        yield wm
 
-def test_get_hello(wm_server): # (4)
+
+def test_get_hello(wm_server):  # (4)
 
     resp1 = requests.get(wm_server.get_url("/hello"))
 
     assert resp1.status_code == 200
-    assert resp1.json() == {"response":"hello"}
+    assert resp1.json() == {"response": "hello"}
 
-def test_get_world(wm_server): # (4)
+
+def test_get_world(wm_server):  # (4)
 
     resp1 = requests.get(wm_server.get_url("/world"))
 
     assert resp1.status_code == 200
-    assert resp1.json() == {"response":"world"}
+    assert resp1.json() == {"response": "world"}
